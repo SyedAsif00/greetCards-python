d82e90aaea8825eda92e6ae6a538773d4ec18e05
Syed Asifullah <syedasifullah00@gmail.com>
2024-05-14 19:10:16 +0500

    updated the code for creating sets based on white or nonWhite margins/borders, dynamially creating sets by taking a white card along with it's cardBody, a single page from the inner envelope, single page from the outer envelope- merging them all together to create a set.

--- cards.py
+++ cards.py
@@ -1,21 +1,22 @@
+import os
 import fitz  
 from PIL import Image
 import numpy as np
 
 def is_white(margin, threshold=0.9):
+    """Determine if an image area is predominantly white."""
     white_pixels = np.all(margin >= 100, axis=-1)
     white_ratio = np.mean(white_pixels)
     return white_ratio > threshold
 
 def analyze_odd_pages(pdf_path, margin=100):
+    """Analyze odd pages to determine if they are predominantly white."""
     document = fitz.open(pdf_path)
     results = {}
     for i in range(0, len(document), 2):
         page = document.load_page(i)
         pix = page.get_pixmap()
-        
         img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-        
         image_np = np.array(img)
 
         top_margin = image_np[:margin, :]
@@ -31,12 +32,37 @@ def analyze_odd_pages(pdf_path, margin=100):
         ])
 
         results[f'Page {i+1}'] = 'White' if all_white else 'Not White'
-
     document.close()
-    
     return results
 
-pdf_path = "./cards-2 (10).pdf"
+def extract_and_assemble(card_pdf, inner_pdf, outer_pdf, results, white_folder, non_white_folder):
+    """Extract and assemble the PDFs based on the analysis results."""
+    card_doc = fitz.open(card_pdf)
+    inner_doc = fitz.open(inner_pdf)
+    outer_doc = fitz.open(outer_pdf)
 
-results = analyze_odd_pages(pdf_path)
-print(results)
+    for set_number, i in enumerate(range(0, len(card_doc), 2)):
+        set_pdf = fitz.open()
+        set_pdf.insert_pdf(card_doc, from_page=i, to_page=i+1)
+
+        set_pdf.insert_pdf(inner_doc, from_page=set_number, to_page=set_number)
+
+        set_pdf.insert_pdf(outer_doc, from_page=0, to_page=0)
+
+        save_path = white_folder if results[f'Page {i+1}'] == 'White' else non_white_folder
+        filename = os.path.join(save_path, f'set_{set_number + 1}.pdf')
+        set_pdf.save(filename)
+        set_pdf.close()
+
+    card_doc.close()
+    inner_doc.close()
+    outer_doc.close()
+
+cards_pdf = './cards-2 (10).pdf'
+inner_envelopes_pdf = './inner envelopes-2 (1).pdf'
+outer_envelopes_pdf = './outer envelopes-2 (1).pdf'
+white_folder = './whiteFolder'
+non_white_folder = './nonWhiteFolder'
+
+results = analyze_odd_pages(cards_pdf)
+extract_and_assemble(cards_pdf, inner_envelopes_pdf, outer_envelopes_pdf, results, white_folder, non_white_folder)
